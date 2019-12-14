1b8d2d1609a73b959e26ebf8d3d093c7aa2210aa
Akhil Kondury <akhil.kondury@gmail.com>
2019-12-14 12:28:51 -0500

    chnages in Teleop Code after today's 1st round of testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/R2_Skystone1_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/R2_Skystone1_TeleOp.java
@@ -39,15 +39,10 @@ public class R2_Skystone1_TeleOp extends R_2_OpMode {
 
         while (opModeIsActive()) {
 
-           // foundationServo1.setPosition(1);
-          //  foundationServo2.setDirection(Servo.Direction.REVERSE);
-         //   foundationServo2.setPosition(1);
-
             setDriveMotorsMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             setArmMotorsMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             extensionMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-            // make sure color sensor servos not open
             loopCount++;
 
             // right stick is throttle
@@ -61,42 +56,21 @@ public class R2_Skystone1_TeleOp extends R_2_OpMode {
             rightPower = motorPower - steering;
 
             //if the a button is pressed then toggle if you are in slowMode
+
             if (this.gamepad1.a) {
                 slowMode = !slowMode;
             }
-//            if (this.gamepad1.b)
-//            {
-//                //raiseArm();
-//                driveStraight(0.5,10.0);
-//            }
 
-            if (slowMode) {
-                rightPower = rightPower/2.0;
-                leftPower = leftPower/2.0;
+            if (this.gamepad1.left_trigger>0){
+                rightPower = rightPower / 4.0;
+                leftPower = leftPower / 4.0;
+            }
+            else if (this.gamepad1.right_trigger > 0) {
+                rightPower = rightPower / 2.0;
+                leftPower = leftPower / 2.0;
             }
 
-            //armpower
-            // Move arm up and down
-          /*  if (this.gamepad1.left_bumper) {
-                // move arm up
-                leftArmMotor.setPower(0.5);
-                rightArmMotor.setPower(0.5);
-            } else if (this.gamepad1.right_bumper) {
-                // move arm down
-                rightArmMotor.setPower(-0.5);
-                leftArmMotor.setPower(-0.5);
-            } else if (this.gamepad2.left_bumper) {
-                // move arm up
-                leftArmMotor.setPower(0.5);
-                rightArmMotor.setPower(0.5);
-            } else if (this.gamepad2.right_bumper) {
-                // move arm down
-                rightArmMotor.setPower(-0.5);
-                leftArmMotor.setPower(-0.5);
-            } else {
-                rightArmMotor.setPower(0.0);
-                leftArmMotor.setPower(0.0);
-            } */
+
 
             if (this.gamepad2.y) {
                 loadBucket();
@@ -106,11 +80,9 @@ public class R2_Skystone1_TeleOp extends R_2_OpMode {
                 stopServo();
             }
 
-
             // make sure we dont make the power too low/high
             leftPower = Range.clip(leftPower, -1, 1.5);
             rightPower = Range.clip(rightPower, -1, 1.5);
-
 
             frontLeftMotor.setPower(-rightPower);
             backLeftMotor.setPower(-rightPower);
@@ -144,9 +116,9 @@ public class R2_Skystone1_TeleOp extends R_2_OpMode {
             }
 
             if (this.gamepad2.dpad_up) {
-                extensionMotor.setPower(-0.75);
+                extensionMotor.setPower(-0.85);
             } else if (this.gamepad2.dpad_down) {
-                extensionMotor.setPower(0.5);
+                extensionMotor.setPower(0.75);
             } else {
                 extensionMotor.setPower(0);
             }
